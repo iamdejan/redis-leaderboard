26242f5f01efe5e108b917692248186a8f8715a3
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-12-17 21:49:10 +0700

    Add example routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+import 'reflect-metadata';
 import express from 'express';
+import bodyParser from "body-parser";
+import {plainToClass} from "class-transformer";
+import Player from "./Player";
+
 const app = express();
 const PORT = 8888;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send("Express + TypeScript Server"));
+app.post("/players", (req, res) => {
+  const player: Player = plainToClass(Player, req.body as Player);
+  res.json(player);
+});
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
